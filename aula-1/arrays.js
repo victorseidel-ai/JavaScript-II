b2776658a812b2c7780b2d8b4f30d09ecf223692
victorseidel-ai <victor.seidel@escola.pr.gov.br>
2026-04-25 13:46:36 +0000

    atividade 1 finalizada

---
+++ aula-1/arrays.js
@@ -0,0 +1,51 @@
+const edRoberto = [
+  {
+  titulo: "Esse Cara Sou Eu",
+  preco: 2
+  },
+  {
+    titulo: "O Calhambeque",
+    preco: 68
+  },
+  {
+    titulo: "Splish Splash",
+    preco: 80
+  },
+  {
+    titulo: "Amigo",
+    preco: 187
+  },
+  {
+    titulo: "Jovens Tardes de Domingo",
+    preco: 195
+  }
+]
+
+const edCarlos = [
+  {
+    titulo: "Detalhes",
+    preco: 200
+  },
+  {
+    titulo: "120... 150... 200 km por Hora",
+    preco: 255
+  },
+  {
+    titulo: "Canta Para a Juventude",
+    preco: 275
+  },
+  {
+    titulo: "Quero Que Vá Tudo Pro Inferno",
+    preco: 380
+  },
+  {
+    titulo: "Canzone Per Te",
+    preco: 667
+  },
+  {
+    titulo: "A Montanha",
+    preco: 1.950
+  }
+]
+
+module.exports = { edRoberto, edCarlos }
