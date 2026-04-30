3f2e226408e27e5ef5e23444d8595c538dd21e9f
victorseidel-ai <victor.seidel@escola.pr.gov.br>
2026-04-30 13:55:58 +0000

    atividade 2 finalizada

---
+++ aula-2/array.js
@@ -0,0 +1,48 @@
+const listaLivros = [
+  {
+    titulo: "Esse Cara Sou Eu",
+    preco: 2
+  },
+  {
+    titulo: "O Calhambeque",
+    preco: 68
+  },
+  {
+    titulo: "Splish Splash",
+    preco: 80
+  },
+  {
+  titulo: "Amigo",
+  preco: 187
+  },
+  {
+    titulo: "Jovens Tardes de Domingo",
+    preco: 195
+  },
+  {
+    titulo: "Detalhes",
+    preco: 200
+  },
+  {
+    titulo: "120... 150... 200 km por Hora",
+    preco: 255
+  },
+  {
+    titulo: "Ruby",
+    preco: 28
+  },
+  {
+    titulo: "JavaScript",
+    preco: 25
+  },
+  {
+    titulo: "Canta Para a Juventude",
+    preco: 275
+  },
+  {
+    titulo: "Canzone Per Te",
+    preco: 667
+  },
+]
+
+module.exports = listaLivros
